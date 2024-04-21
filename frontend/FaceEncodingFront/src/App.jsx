a662a9dacc35a6ee44b862e47fdca944659e5fb2
RammonZub <118915883+RammonZub@users.noreply.github.com>
2024-04-21 18:04:45 +0200

    vercer json added

--- frontend/FaceEncodingFront/src/App.jsx
+++ frontend/FaceEncodingFront/src/App.jsx
@@ -1,20 +1,15 @@
-import './App.css'
-import { Route } from 'react-router-dom'
-import { BrowserRouter, Routes } from 'react-router-dom'
-import VideoStream from './components/StreamVideo'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import VideoStream from './components/StreamVideo';
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<VideoStream />} />
-        
-
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
